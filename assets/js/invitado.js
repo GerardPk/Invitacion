98278b5925b32b998e91fef09540a06d1ccb8e91
GerardPk <42327153+GerardPk@users.noreply.github.com>
2026-07-09 14:18:43 -0600

    Script

---
+++ assets/js/invitado.js
@@ -0,0 +1,45 @@
+const params =
+  new URLSearchParams(
+    window.location.search
+  );
+
+const token =
+  params.get("token");
+
+const API =
+"https://script.google.com/macros/s/AKfycbw8CREdG0D2joBa_VEoVBoxUPiCAfDgCmpfTBfaGidG_2lAAAyxNzMr4w0SmklwPFIN/exec";
+
+async function cargarInvitado(){
+
+  const response =
+    await fetch(
+      ${API}?token=${token}
+    );
+
+  const data =
+    await response.json();
+
+  if(data.error){
+
+    document.body.innerHTML =
+      "<h1>Invitación inválida</h1>";
+
+    return;
+  }
+
+  document
+    .getElementById(
+      "invitado"
+    )
+    .innerHTML =
+    Hola ${data.nombre};
+
+  document
+    .getElementById(
+      "boletos"
+    )
+    .innerHTML =
+    Tienes ${data.boletos} boletos disponibles;
+}
+
+cargarInvitado();
